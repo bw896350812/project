9f3d1547515673b013810cc46e1193ddf2ff385a
tzh <896350812@qq.com>
2019-09-04 13:38:49 +0800

    dddd

--- src/main/java/com/bw/controller/MovieController.java
+++ src/main/java/com/bw/controller/MovieController.java
@@ -22,6 +22,8 @@ public class MovieController {
 	public String list(Model model) {
 		List<Map<String,Object>> shopList = movieService.getShopList();
 		model.addAttribute("shopList", shopList);
+		model.addAttribute("shopList", shopList);
+		model.addAttribute("shopList", shopList);
 		return "list";
 	}
 	
